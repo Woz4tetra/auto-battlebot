faa8631d8ddd5d30f1723ba7f74a9d17dc567f67
Woz4tetra <woz4tetra@gmail.com>
2026-01-07 00:23:36 -0500

    remove opencv from base packages. move to ubuntu 24 packages

--- scripts/install_ubuntu_24.sh
+++ scripts/install_ubuntu_24.sh
@@ -15,5 +15,6 @@ source "$PROJECT_ROOT/install/build_cpp_project.sh"
 # Run checks and installation
 check_os_ubuntu_24
 install_packages "$PROJECT_ROOT/install/base_packages.txt"
+install_packages "$PROJECT_ROOT/install/ubuntu_24_packages.txt"
 install_libtorch_ubuntu 'https://download.pytorch.org/libtorch/cu130/libtorch-shared-with-deps-2.9.1%2Bcu130.zip'
 build_cpp_project "$@"
