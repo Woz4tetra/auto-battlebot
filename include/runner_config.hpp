60e767b0daeea53cdf1a4fd0cdf24b69aa57c257
Woz4tetra <woz4tetra@gmail.com>
2026-03-21 22:37:30 -0400

    less confusing status messages

--- include/runner_config.hpp
+++ include/runner_config.hpp
@@ -9,8 +9,9 @@
 namespace auto_battlebot {
 struct RunnerConfiguration {
     double max_loop_rate;  // Hz
+    int default_opponent_count;
 
-    RunnerConfiguration() : max_loop_rate(300.0) {}
+    RunnerConfiguration() : max_loop_rate(300.0), default_opponent_count(1) {}
 };
 
 void load_runner_from_toml(toml::table const &toml_data, std::vector<std::string> &parsed_sections,
